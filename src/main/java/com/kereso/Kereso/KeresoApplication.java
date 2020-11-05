828a09ce40fad6448c34409894cfcc14d0c521ed
Javexify <raksanyirobi@gmail.com>
2020-11-05 21:25:39 +0100

    Added spring-web dependency

--- src/main/java/com/kereso/Kereso/KeresoApplication.java
+++ src/main/java/com/kereso/Kereso/KeresoApplication.java
@@ -2,12 +2,18 @@ package com.kereso.Kereso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class KeresoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KeresoApplication.class, args);
 	}
-
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 }
